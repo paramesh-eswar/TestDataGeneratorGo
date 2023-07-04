5f0229b2ac42a7fd80f0fce26b91c3efee831ea9
paramesh-eswar <paramesh.eswar@gmail.com>
2023-07-04 22:43:39 +0530

    init ui

--- src/main/main.go
+++ src/main/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 
 	"fyne.io/fyne/v2"
@@ -21,6 +20,7 @@ func init() {
 }
 
 var topWindow fyne.Window
+var metadataFileName string
 
 func main() {
 	a := app.New()
@@ -28,7 +28,7 @@ func main() {
 	// a.SetIcon(theme.TdgLogo())
 	a.SetIcon(tdgIcon)
 	// a.Settings().SetTheme(theme.MyTdgTheme{})
-	logLifecycle(a)
+	// logLifecycle(a)
 	w := a.NewWindow("Test Data Generator")
 	w.Resize(fyne.NewSize(600, 400))
 	w.SetMaster()
@@ -39,8 +39,6 @@ func main() {
 	title.TextStyle.Bold = true
 	titleContainer := container.NewCenter(title)
 
-	var fileContents string
-	var fileName string
 	var fileChooser *dialog.FileDialog
 
 	numOfRowsLbl := widget.NewRichText(&widget.TextSegment{Text: "Number of rows", Style: widget.RichTextStyle{Alignment: fyne.TextAlignTrailing, ColorName: widget.RichTextStyleCodeInline.ColorName}})
@@ -51,31 +49,42 @@ func main() {
 	metadataFileEntry := widget.NewEntry()
 	metadataFileEntry.Disable()
 	metadataFileEntry.SetPlaceHolder("Metadata file path")
+	fileChooser = dialog.NewFileOpen(func(r fyne.URIReadCloser, _ error) {
+		// data, _ := ioutil.ReadAll(r)
+		// result := fyne.NewStaticResource("name", data)
+		// fileContents := string(result.StaticContent)
+		fileName := r.URI().Path()
+		// log.Print("inside:" + fileName)
+		metadataFileEntry.SetText(fileName)
+		persistFileName(fileName)
+	}, w)
+	fileChooser.SetFilter(storage.NewExtensionFileFilter([]string{".json"}))
+
 	fileUploadBtn := widget.NewButton("Choose file", func() {
-		fileChooser = dialog.NewFileOpen(func(r fyne.URIReadCloser, _ error) {
-			data, _ := ioutil.ReadAll(r)
-			result := fyne.NewStaticResource("name", data)
-			fileContents = string(result.StaticContent)
-			fileName = r.URI().Path()
-			// log.Print(fileName)
-			// log.Print(fileContents)
-			// fw := fyne.CurrentApp().NewWindow(string(result.StaticName))
-			// fw.SetContent(container.NewScroll(resultArea))
-			// fw.Show()
-		}, w)
-		fileChooser.SetFilter(storage.NewExtensionFileFilter([]string{".json"}))
 		fileChooser.Show()
-		log.Print(fileName)
-		log.Print(fileContents)
 	})
 	// fileUploadBtn.Resize(fyne.Size{Width: 10, Height: 30})
 
+	generateDataBtn := widget.NewButton("Generate Data", func() {
+		log.Print(metadataFileName)
+	})
+
 	// title.Move(fyne.NewPos(20, 20))
 	// content := container.NewBorder(titleContainer, nil, nil, nil, nil)
-	content := container.New(layout.NewVBoxLayout(), titleContainer, container.New(layout.NewAdaptiveGridLayout(3), numOfRowsLbl, numOfRowsEntry, layout.NewSpacer(), metadataFileLbl, metadataFileEntry, fileUploadBtn))
+	content := container.New(layout.NewVBoxLayout(), titleContainer, container.New(
+		layout.NewAdaptiveGridLayout(3),
+		numOfRowsLbl, numOfRowsEntry, layout.NewSpacer(),
+		metadataFileLbl, metadataFileEntry, fileUploadBtn,
+		layout.NewSpacer(), generateDataBtn, layout.NewSpacer(),
+	),
+	)
 	w.SetContent(content)
 	w.SetFixedSize(true)
 	w.ShowAndRun()
+}
+
+func persistFileName(fileName string) {
+	metadataFileName = fileName
 }
 
 func logLifecycle(a fyne.App) {
